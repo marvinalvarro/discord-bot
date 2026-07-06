a6f8ccf4290c66d7a2392947b867e5df48868de7
Umar Hadyu Al Fajri <marvinalghifari123@gmail.com>
2026-07-07 04:50:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 require("dotenv").config();
 
 console.log("TOKEN:", process.env.TOKEN);
@@ -8,12 +7,4 @@ module.exports = {
     token: process.env.TOKEN,
     prefix: process.env.PREFIX,
     clientId: process.env.CLIENT_ID,
-=======
-require("dotenv").config();
-
-module.exports = {
-    token: process.env.TOKEN,
-    prefix: process.env.PREFIX,
-    clientId: process.env.CLIENT_ID,
->>>>>>> 95a57b2088df19726c4d831d6ae1699863e90c31
 };
